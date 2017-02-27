47a1c676672fbde68f7276c62096bfac30dae285
priyankaggarwal <priyankaggarwal2015@gmail.com>
2017-02-27 22:28:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req, res) {
-  res.send("Article one requested");
-});
-
-app.get('/article-two',function (req, res) {
-  res.send("Article two requested");
-});
-
-app.get('/article-three',function (req, res) {
-  res.send("Article three requested");
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+      res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
